f411931752a21422048b9795b358ba031988440f
ggcoders <116106531+ggcoders@users.noreply.github.com>
2025-01-30 14:53:11 +0000

    new change to sbheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 
 if __name__ == "__main__":
     st.title("DATALIZE")
-    st.subheader("Visualizing data made simple")
+    st.subheader("Visualizing data, made simple")
     
     f = st.file_uploader(label="Please input your file here to continue") # allow the user to upload the file  
     "---"
